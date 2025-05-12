aa4575f538b74307c1eccbd3923c71f18ef749b5
Darren <Darren_Serious.hotmail.com>
2025-05-12 12:29:09 +0800

    feat: add build package funciton

--- scripts/build_protobuf.sh
+++ scripts/build_protobuf.sh
@@ -4,7 +4,7 @@ source ${SCRIPTS_DIR}/utils.sh
 
 check_in_docker $0
 
-GIT_URL=https://github.com/grpc/grpc.git
-GIT_TAG=v1.71.1
+GIT_URL=https://github.com/protocolbuffers/protobuf.git
+GIT_TAG=v4.25.7
 
 cmake_build_repo ${GIT_URL} ${GIT_TAG} ${SCRIPTS_DIR}
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -6,3 +6,22 @@ check_in_docker() {
         exit 1
     fi
 }
+
+cmake_build_repo() {
+    local GIT_URL=$1
+    local GIT_TAG=$2
+    local SCRIPTS_DIR=$3
+    local WORK_DIR=$(cd "${SCRIPTS_DIR}/.." && pwd)
+    local REPO_NAME=$(basename -s .git ${GIT_URL})
+    local SOURCE_DIR=${WORK_DIR}/_deps/${REPO_NAME}-src
+    local BUILD_DIR=${WORK_DIR}/_deps/${REPO_NAME}-build
+
+    mkdir -p ${SOURCE_DIR}
+    mkdir -p ${BUILD_DIR}
+
+    git clone ${GIT_URL} -b ${GIT_TAG} --recurse-submodules ${SOURCE_DIR}
+    cmake -B ${BUILD_DIR} -S ${SOURCE_DIR} -DCMAKE_INSTALL_PREFIX=${BUILD_DIR}/install
+    cmake --build ${BUILD_DIR} -- -j10
+    cmake --install ${BUILD_DIR}
+    tar -czvf ${WORK_DIR}/${REPO_NAME}_${GIT_TAG}_$(git branch --show-current)_$(uname -m).tar.gz -C ${BUILD_DIR}/install .
+}
